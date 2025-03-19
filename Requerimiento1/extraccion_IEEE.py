1db716ea96a2cebd393bb5cdffba28a94f14b403
JhonatanGV03 <jhonatanva.2003.jsgv@gmail.com>
2025-03-19 07:17:28 -0500

    Modificación del main

--- Requerimiento1/extraccion_IEEE.py
+++ Requerimiento1/extraccion_IEEE.py
@@ -1,6 +1,5 @@
 import os
 import time
-import re
 
 from Requerimiento1 import automatizacion
 
--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from Requerimiento1.arreglar_ieee import procesar_archivos_ieee
 from Requerimiento1.unificarArchivos import unificar_filtrar_archivos
 
 def main():
-    #descargar_citas()
-    #procesar_archivos_ieee()
-    unificar_filtrar_archivos()
 
-
+    descargar_citas()  # ---> Descarga las citas de las bases de datos Sage, IEEE y ScienceDirect de manera automatizada
+    procesar_archivos_ieee() # ---> Arregla las entradas IEEE en formato BibTeX (Saltos de linea)
+    unificar_filtrar_archivos() # ---> Unifica y filtra las citas descargadas de las bases de datos
 
 if __name__ == "__main__":
     main()
